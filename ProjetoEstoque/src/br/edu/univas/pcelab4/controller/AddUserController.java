e6460c0801530bfe2e5a865e26b2de404f5e6778
Denis Barbosa <denis_barbosa123@hotmail.com>
2018-11-10 17:38:12 -0200

    Adicionado tela de adm e tela de cadastro de cargo

--- ProjetoEstoque/src/br/edu/univas/pcelab4/controller/AddUserController.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/controller/AddUserController.java
@@ -7,12 +7,14 @@ import javax.swing.JTextField;
 
 import br.edu.univas.pcela4.listener.CadastraUsuarioListener;
 import br.edu.univas.pcelab4.dao.UserDAO;
+import br.edu.univas.pcelab4.model.Endereco;
 import br.edu.univas.pcelab4.model.Usuario;
 import br.edu.univas.pcelab4.view.CadastroUsuarioFrame;
 
 public class AddUserController {
 	private UserDAO dao;
 	private Usuario usuario;
+	private Endereco endereco;
 	private CadastroUsuarioFrame frame;
 	
 	public AddUserController(){
--- ProjetoEstoque/src/br/edu/univas/pcelab4/main/Runner.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/main/Runner.java
@@ -6,7 +6,7 @@ public class Runner {
 
 	public static void main(String[] args) {
 		MainController controller = new MainController();
-		controller.exibirTelaCadUsuario();
+		controller.abrirTelaAdm();
 
 	}
 
