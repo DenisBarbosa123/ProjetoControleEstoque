11cae42daef4d9ad9664c0a04e7f649463c40163
Denis Barbosa <denis_barbosa123@hotmail.com>
2018-11-10 11:37:58 -0200

    Adicionando componentes na tela de cadastro de usuario

--- ProjetoEstoque/src/br/edu/univas/pcelab4/controller/MainController.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/controller/MainController.java
@@ -1,12 +1,15 @@
 package br.edu.univas.pcelab4.controller;
 
+import br.edu.univas.pcelab4.model.Usuario;
 import br.edu.univas.pcelab4.view.CadastroUsuarioFrame;
 
 public class MainController {
 	CadastroUsuarioFrame cadUsuario;
+	Usuario usuario;
 	
 	public MainController(){
 		cadUsuario = new CadastroUsuarioFrame();
+		usuario = new Usuario();
 	}
 	
 	public void exibirTelaCadUsuario(){
--- ProjetoEstoque/src/br/edu/univas/pcelab4/view/AdministradorFrame.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/view/AdministradorFrame.java
@@ -1,5 +1,5 @@
 package br.edu.univas.pcelab4.view;
 
 public class AdministradorFrame {
-
+	
 }
