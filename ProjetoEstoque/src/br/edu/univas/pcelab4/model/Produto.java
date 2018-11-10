fc15743c6b7560c05d702f6a8bdf609763dd1020
Denis Barbosa <denis_barbosa123@hotmail.com>
2018-11-09 23:13:15 -0200

    Adicionando getters e setters nas classes model

--- ProjetoEstoque/src/br/edu/univas/pcelab4/model/Produto.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/model/Produto.java
@@ -7,4 +7,35 @@ public class Produto {
 	private int qtdeMinima;
 	private double valor;
 	
+	public int getCodigoProduto() {
+		return codigoProduto;
+	}
+	public void setCodigoProduto(int codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQtde() {
+		return qtde;
+	}
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+	public int getQtdeMinima() {
+		return qtdeMinima;
+	}
+	public void setQtdeMinima(int qtdeMinima) {
+		this.qtdeMinima = qtdeMinima;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
 }
--- ProjetoEstoque/src/br/edu/univas/pcelab4/model/Usuario.java
+++ ProjetoEstoque/src/br/edu/univas/pcelab4/model/Usuario.java
@@ -7,5 +7,41 @@ public class Usuario {
 	private String password;
 	private String email;
 	private String telefone;
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 	
 }
